7cc2f3e521d608e5194f2880c0d59353845e9feb
Antoine Balaine <abalaineaccts@gmail.com>
2024-05-05 17:32:22 -0400

    Completion: update trigger characters

--- src/providers/lua/completion.ts
+++ src/providers/lua/completion.ts
@@ -1,30 +1,32 @@
-import * as vscode from "vscode";
+import * as vscode from "vscode"
 
 export class DebugSnippetCompletionItemProvider implements vscode.CompletionItemProvider {
-  public static triggerCharacters = ['D', 'DE', 'DEB', 'DEBU', 'DEBUG']
+  public static triggerCharacters = ["D", "E", "B", "U", "G"]
 
-  private snippet: vscode.CompletionItem;
+  private snippet: vscode.CompletionItem
   constructor(private extensionPath: string) {
-    const debugSnippetCompletionItem = new vscode.CompletionItem("reaper_debug", vscode.CompletionItemKind.Snippet);
+    const debugSnippetCompletionItem = new vscode.CompletionItem(
+      "REAPER DEBUGGER",
+      vscode.CompletionItemKind.Snippet
+    )
 
-    debugSnippetCompletionItem.insertText = new vscode.SnippetString(`local VSDEBUG = dofile("${this.extensionPath}/debugger/LoadDebug.lua")$1`);
+    debugSnippetCompletionItem.insertText = new vscode.SnippetString(
+      `local VSDEBUG = dofile("${this.extensionPath}/debugger/LoadDebug.lua")$1`
+    )
     this.snippet = debugSnippetCompletionItem
   }
 
-
   provideCompletionItems(
     document: vscode.TextDocument,
-    position: vscode.Position,
+    position: vscode.Position
   ): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList> {
-
-
     // Check if the user is typing 'DEBUG'
-    const range = document.getWordRangeAtPosition(position, /DE?B?U?G?/);
+    const range = document.getWordRangeAtPosition(position, /DE?B?U?G?/)
     if (range) {
-      return [this.snippet];
+      return [this.snippet]
     }
 
-    return [];
+    return []
   }
 
   register() {
